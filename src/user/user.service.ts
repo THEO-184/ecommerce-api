942aa19e0c8227ee033910a328812e616409e834
TheophilusBoakye <theophilus.yiadom@amalitech.com>
2023-08-25 15:39:05 +0000

    prisma service setup

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/user.dto';
 import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  constructor() {}
+  constructor(private prisma: PrismaService) {}
 
   async createUser(payload: CreateUserDto) {
     const salt = await bcrypt.genSalt(10);
